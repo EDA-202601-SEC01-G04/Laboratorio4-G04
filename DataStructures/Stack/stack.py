bb731f847c4abe12d34b19d9467111674499d733
sof-ortiz <s.ortizo2@uniandes.edu.co>
2026-02-13 07:37:34 -0500

    Stack 1

---
+++ DataStructures/Stack/stack.py
@@ -0,0 +1,22 @@
+def new_stack():
+    my_stack =  {'size': 0, 
+      'first': None, 
+      'last': None}
+    return my_stack 
+
+def push(my_stack, element):
+    return my_stack 
+
+def pop(my_stack):
+    return my_stack 
+
+def is_empty(my_stack):
+    empty = True
+    return empty
+
+def top(my_stack):
+    return my_stack
+
+def size(my_stack):
+    size = 0
+    return size
